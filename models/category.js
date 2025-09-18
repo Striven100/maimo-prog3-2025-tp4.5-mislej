bf2201d0dcdcc873f1b8f1bee4a36aa2e5e2f632
Tobias Mislej <tobimislej@gmail.com>
2025-09-18 19:04:35 -0300

    cambios menores

--- models/category.js
+++ models/category.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const {Schema} = mongoose.Schema;
+const {Schema} = mongoose;
 
 const categorySchema = new Schema({
   name: { type: String, required: true, trim: true },
